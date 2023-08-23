3a2c18d9d4b037279b8b37c43a925ee8d4437f7e
Isaac Mhlanga <isaac.mhlanga13@gmail.com>
2023-08-23 21:41:48 +0200

    removed a class

--- src/components/partials/Project.jsx
+++ src/components/partials/Project.jsx
@@ -44,8 +44,8 @@ function Projects(props) {
                   <div class="preview-card__title text-white dark:text-white">
                     {projectName}
                   </div>
-                  <div className="ml-5 mr-5">
-                    <p class="preview-card__text text-gray-500 dark:text-gray-400">
+                  <div className="border border-lg">
+                    <p class="text-gray-500 dark:text-gray-400">
                       {projectDescription}
                     </p>
                   </div>
