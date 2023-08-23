d1e30b613c2e43832ad2e38d023f3685baccba5e
Isaac Mhlanga <isaac.mhlanga13@gmail.com>
2023-08-23 22:31:25 +0200

    save all changes

--- src/components/partials/Project.jsx
+++ src/components/partials/Project.jsx
@@ -44,7 +44,7 @@ function Projects(props) {
                   <div class="preview-card__title text-white dark:text-white">
                     {projectName}
                   </div>
-                  <div class="preview-card__text text-gray-500 dark:text-gray-400">
+                  <div className="preview-card__text text-gray-500 sm:p-20 dark:text-gray-400">
                     {projectDescription}
                   </div>
 
